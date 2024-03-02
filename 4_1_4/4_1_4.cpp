216b7ba08427e5109892f12f44f3858585a85408
Alexander <shalagin.aiu@phystech.edu>
2024-03-02 22:11:35 +0300

    Добавьте файлы проекта.

---
+++ 4_1_4/4_1_4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int divisor = 2;
+    int divisible = N;
+    string number = "";
+
+    while (divisible != 0)
+    {
+        number = to_string(divisible % divisor) + number; // остаток
+        divisible /= divisor;
+        divisor++;
+
+    }
+
+    cout << number << endl;
+
+
+}
